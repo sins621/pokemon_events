cc3f36cf21531d04d76713ebeb49159667a0debc
bradly-warp <bradly@warpdevelopment.com>
2025-05-12 16:56:19 +0200

    feat: calendar controller

---
+++ Events.Api/Controllers/CalendarEventsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Events.Api.Data;
+using Events.Api.Models;
+
+namespace Events.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalendarEventsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CalendarEventsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CalendarEvents
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CalendarEvent>>> GetCalenderEvents()
+        {
+            return await _context.CalenderEvents.ToListAsync();
+        }
+
+        // GET: api/CalendarEvents/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CalendarEvent>> GetCalendarEvent(int id)
+        {
+            var calendarEvent = await _context.CalenderEvents.FindAsync(id);
+
+            if (calendarEvent == null)
+            {
+                return NotFound();
+            }
+
+            return calendarEvent;
+        }
+
+        // PUT: api/CalendarEvents/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCalendarEvent(int id, CalendarEvent calendarEvent)
+        {
+            if (id != calendarEvent.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(calendarEvent).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CalendarEventExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CalendarEvents
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CalendarEvent>> PostCalendarEvent(CalendarEvent calendarEvent)
+        {
+            _context.CalenderEvents.Add(calendarEvent);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCalendarEvent", new { id = calendarEvent.Id }, calendarEvent);
+        }
+
+        // DELETE: api/CalendarEvents/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCalendarEvent(int id)
+        {
+            var calendarEvent = await _context.CalenderEvents.FindAsync(id);
+            if (calendarEvent == null)
+            {
+                return NotFound();
+            }
+           
+            _context.CalenderEvents.Remove(calendarEvent);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CalendarEventExists(int id)
+        {
+            return _context.CalenderEvents.Any(e => e.Id == id);
+        }
+    }
+}
