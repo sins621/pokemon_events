b84d4ae3d9b9f5aa4c1d67b2a5b5b21fccf76e43
Bradly Carpenter <bradly@warpdevelopment.com>
2025-05-14 17:30:23 +0200

    feat: serialize month dayjs object

--- events-frontend/src/components/calendar/month-view/month-view.tsx
+++ events-frontend/src/components/calendar/month-view/month-view.tsx
@@ -2,15 +2,19 @@ import { Fragment } from "react";
 import MonthViewBox from "./MonthViewBox";
 import { RootState } from "@/store";
 import { useSelector } from "react-redux";
+import dayjs from "dayjs";
 
 const MonthView: React.FC = () => {
   const twoDMonthArray = useSelector(
     (state: RootState) => state.calendarData.twoDMonthArray,
   );
+  const parsedTwoDMonthArray = twoDMonthArray.map((week) =>
+    week.map((day) => dayjs(day)),
+  );
 
   return (
     <section className="grid grid-cols-7 grid-rows-5 lg:h-[100vh]">
-      {twoDMonthArray.map((row, i) => (
+      {parsedTwoDMonthArray.map((row, i) => (
         <Fragment key={i}>
           {row.map((day, j) => (
             <MonthViewBox key={j} day={day} rowIndex={i} />
