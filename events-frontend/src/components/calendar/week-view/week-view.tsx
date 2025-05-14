6138b9bfb7aa45b4b1c2731ac7f40fd169586874
Bradly Carpenter <bradly@warpdevelopment.com>
2025-05-14 23:00:08 +0200

    save: week-view
    - added week day numbers
    - added day highlighting
    - added week day words
    - added week day word highlighting

--- events-frontend/src/components/calendar/week-view/week-view.tsx
+++ events-frontend/src/components/calendar/week-view/week-view.tsx
@@ -1,15 +1,57 @@
 import { getWeekDays } from "@/lib/getTime";
+import { cn } from "@/lib/utils";
 import { RootState } from "@/store";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const WeekView: React.FC = () => {
   const userSelectedDate = dayjs(
     useSelector((state: RootState) => state.calendarData.userSelectedDate),
   );
-  console.log("Parsed Date: ", userSelectedDate.toISOString());
+
   const Days = getWeekDays(userSelectedDate);
-  return <div></div>;
+
+  const [currentTime, setCurrentTime] = useState(dayjs());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(dayjs());
+    }, 60000);
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <>
+      <div className="grid grid-cols-[auto_1fr_1fr_1fr_1fr_1fr_1fr_1fr] place-items-center px-4 py-2">
+        <div className="w-16 border-r border-gray-300">
+          <div className="relative h-16">
+            <div className="absolute top-2 text-xs text-gray-600">GMT + 2</div>
+          </div>
+        </div>
+        {Days.map(({ currentDate, today }, index) => (
+          <div key={index} className="flex flex-col items-center">
+            <div className={cn("text-xs", today && "text-blue-600")}>
+              {currentDate.format("ddd")}
+            </div>
+
+            <div
+              className={cn(
+                "h-12 w-12 rounded-full p-2 text-2xl",
+                today && "bg-blue-600 text-white",
+              )}
+            >
+              {currentDate.format("DD")}{" "}
+            </div>
+          </div>
+        ))}
+      </div>
+      <ScrollArea className="h-[70vh]">
+        <div className="grid grid-cols-[auto_1fr_1fr_1fr_1fr_1fr_1fr_1fr] px-4 py-2"></div>
+      </ScrollArea>
+    </>
+  );
 };
 
 export default WeekView;
