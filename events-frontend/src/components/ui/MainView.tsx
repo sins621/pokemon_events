0b68018b95fe1a5a6a8042eda517e60cf2946b8d
Bradly Carpenter <bradly@warpdevelopment.com>
2025-05-14 14:21:52 +0200

    feat: mainview reflects state of view selection

--- events-frontend/src/components/ui/MainView.tsx
+++ events-frontend/src/components/ui/MainView.tsx
@@ -1,12 +1,21 @@
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
+
 import Sidebar from "../calendar/sidebar/Sidebar";
 import MonthView from "../calendar/month-view/month-view";
+import DayView from "../calendar/day-view/day-view";
 
 const MainView: React.FC = () => {
+  const selectedView = useSelector(
+    (state: RootState) => state.calendarView.selectedView,
+  );
+
   return (
     <div className="flex">
-      <Sidebar/>
+      <Sidebar />
       <div className="w-full flex-1">
-        <MonthView />
+        {selectedView === "month" && <MonthView />}
+        {selectedView === "day" && <DayView />}
       </div>
     </div>
   );
