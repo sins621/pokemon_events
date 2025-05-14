d704189966eb8c557334746407db439960e886ae
Bradly Carpenter <bradly@warpdevelopment.com>
2025-05-14 14:40:31 +0200

    feat: getWeekDays function

--- events-frontend/src/lib/getTime.ts
+++ events-frontend/src/lib/getTime.ts
@@ -1,5 +1,9 @@
 import dayjs from "dayjs";
 
+export const isCurrentDay = (day: dayjs.Dayjs) => {
+  return day.isSame(dayjs(), "day");
+};
+
 export const getMonth = (month = dayjs().month()) => {
   const year = dayjs().year();
   const firstDayofMonth = dayjs().set("month", month).startOf("month").day();
@@ -9,4 +13,20 @@ export const getMonth = (month = dayjs().month()) => {
   return Array.from({ length: 5 }, () =>
     Array.from({ length: 7 }, () => dayjs(new Date(year, month, ++dayCounter))),
   );
+};
+
+export const getWeekDays = (date: dayjs.Dayjs) => {
+  const startOfWeek = date.startOf("week");
+
+  const weekDates = [];
+
+  for (let i = 0; i < 7; i++) {
+    const currentDate = startOfWeek.add(i, "day");
+    weekDates.push({
+      currentDate,
+      today:
+        currentDate.toDate().toDateString() === dayjs().toDate().toDateString(),
+      isCurrentDay,
+    });
+  }
 };
