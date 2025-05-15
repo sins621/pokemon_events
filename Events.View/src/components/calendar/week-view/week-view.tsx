f12c089d254e0165215e303c35859c286f04ceed
Bradly Carpenter <bradly@warpdevelopment.com>
2025-05-15 12:43:30 +0200

    feat: weekdays grid

--- Events.View/src/components/calendar/week-view/week-view.tsx
+++ Events.View/src/components/calendar/week-view/week-view.tsx
@@ -1,4 +1,4 @@
-import { getWeekDays } from "@/lib/getTime";
+import { getHours, getWeekDays } from "@/lib/getTime";
 import { cn } from "@/lib/utils";
 import { RootState } from "@/store";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -20,23 +20,24 @@ const WeekView: React.FC = () => {
       setCurrentTime(dayjs());
     }, 60000);
     return () => clearInterval(interval);
-  });
+  }, []);
 
   return (
     <>
       <div className="grid grid-cols-[auto_1fr_1fr_1fr_1fr_1fr_1fr_1fr] place-items-center px-4 py-2">
         <div className="w-16 border-r border-gray-300">
           <div className="relative h-16">
-            <div className="absolute top-2 text-xs text-gray-600">GMT + 2</div>
+            <div className="absolute top-2 text-xs text-gray-600">GMT +2</div>
           </div>
         </div>
-        {Days.map(({ currentDate, today }, index) => (
+
+        {/* Week View Header */}
+
+        {getWeekDays(userSelectedDate).map(({ currentDate, today }, index) => (
           <div key={index} className="flex flex-col items-center">
             <div className={cn("text-xs", today && "text-blue-600")}>
               {currentDate.format("ddd")}
             </div>
-            <div className="flex text-blue-200"></div>
-
             <div
               className={cn(
                 "h-12 w-12 rounded-full p-2 text-2xl",
@@ -48,8 +49,63 @@ const WeekView: React.FC = () => {
           </div>
         ))}
       </div>
+
+      {/* Time Column & Corresponding Boxes of time per each date  */}
+
       <ScrollArea className="h-[70vh]">
-        <div className="grid grid-cols-[auto_1fr_1fr_1fr_1fr_1fr_1fr_1fr] px-4 py-2"></div>
+        <div className="grid grid-cols-[auto_1fr_1fr_1fr_1fr_1fr_1fr_1fr] px-4 py-2">
+          {/* Time Column */}
+          <div className="w-16 border-r border-gray-300">
+            {getHours.map((hour, index) => (
+              <div key={index} className="relative h-16">
+                <div className="absolute -top-2 text-xs text-gray-600">
+                  {hour.format("HH:mm")}
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Week Days Corresponding Boxes */}
+
+          {getWeekDays(userSelectedDate).map(
+            ({ isCurrentDay, today }, index) => {
+              const dayDate = userSelectedDate
+                .startOf("week")
+                .add(index, "day");
+
+              return (
+                <div key={index} className="relative border-r border-gray-300">
+                  {getHours.map((hour, i) => (
+                    <div
+                      key={i}
+                      className="relative flex h-16 cursor-pointer flex-col items-center gap-y-2 border-b border-gray-300 hover:bg-gray-100"
+                      onClick={() => {
+                        // setDate(dayDate.hour(hour.hour()));
+                        // openPopover();
+                      }}
+                    >
+                      {/* <EventRenderer */}
+                      {/*   events={events} */}
+                      {/*   date={dayDate.hour(hour.hour())} */}
+                      {/*   view="week" */}
+                      {/* /> */}
+                    </div>
+                  ))}
+                  {/* Current time indicator */}
+
+                  {isCurrentDay(dayDate) && today && (
+                    <div
+                      className={cn("absolute h-0.5 w-full bg-red-500")}
+                      style={{
+                        top: `${(currentTime.hour() / 24) * 100}%`,
+                      }}
+                    />
+                  )}
+                </div>
+              );
+            },
+          )}
+        </div>
       </ScrollArea>
     </>
   );
